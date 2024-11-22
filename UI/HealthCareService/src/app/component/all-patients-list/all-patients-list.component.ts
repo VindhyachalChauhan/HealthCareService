36138c342bcbf69fd2bbcdff3c2a6ba4ba6855cb
VindhyachalChauhan <vindhyachalchauhan123@gmail.com>
2024-11-22 21:28:53 +0530

    without async

--- UI/HealthCareService/src/app/component/all-patients-list/all-patients-list.component.ts
+++ UI/HealthCareService/src/app/component/all-patients-list/all-patients-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Patient } from 'src/app/models/patient';
+import { PatientService } from 'src/app/services/patient.service';
 
 @Component({
   selector: 'app-all-patients-list',
   templateUrl: './all-patients-list.component.html',
   styleUrls: ['./all-patients-list.component.css']
 })
-export class AllPatientsListComponent {
+export class AllPatientsListComponent implements OnInit {
+ patients?:Patient[]
+  constructor(private patientService:PatientService){
+
+  }
+  ngOnInit(): void {
+
+    this.patientService.getAllPatients()
+    .subscribe({
+      next:(response)=>{
+
+        this.patients=response;
+      }
+    });
+
+  }
 
 }
